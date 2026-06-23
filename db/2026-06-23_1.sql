a36ad7f70661f387f9f2c98f9228a3669e286dde
Nikolay <info@web-nick.com>
2026-06-23 12:00:09 +0300

    Почистить вкладку Заказы в админке: https://github.com/kosatka-org/lsb/issues/31

---
+++ db/2026-06-23_1.sql
@@ -0,0 +1,21 @@
+TRUNCATE order_comments;
+
+TRUNCATE order_delivery_events;
+
+TRUNCATE order_managers;
+
+TRUNCATE orders;
+
+TRUNCATE orders_events;
+
+TRUNCATE orders_payments;
+
+TRUNCATE orders_products;
+
+TRUNCATE orders_services;
+
+TRUNCATE orders_services_history;
+
+TRUNCATE special_orders;
+
+TRUNCATE special_orders_comments;
