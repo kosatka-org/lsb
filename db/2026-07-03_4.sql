1bef6ce469d546947e2a346c51b90495db02ac88
Nikolay <info@web-nick.com>
2026-07-03 13:05:56 +0300

    Почистить базу пользователей: https://github.com/kosatka-org/lsb/issues/48

---
+++ db/2026-07-03_4.sql
@@ -0,0 +1,18 @@
+TRUNCATE TABLE `users`;
+
+INSERT INTO `users` VALUES (1, 14, 1272, 'kostin@lsboutique.ru', 'vkontakte', 'http://vk.com/id3657625', 1, '3375311338cb9d1d1ca68d888a5a6a93', 'Алексей Костин', '3657625', 0, '', '', 0, 'a:3:{s:10:\"category_4\";s:1:\"0\";s:19:\"chooseAllCategories\";s:1:\"0\";s:10:\"category_2\";s:1:\"1\";}', 'a:1:{i:40644;a:1:{i:48;b:1;}}', 2, 0, 'low cost', 'Internet', 1, 1, 0, '2019-06-05 18:30:02', '2016-02-16 09:15:42', 0, 0, '', '', '8909789867985687', '7490694471126521', '79202508074', '', '', 'sklad@lsboutique.ru', 'Москва', 992, 'test', '', '1982-07-21', '2016-03-22', 0, 701491, 'Галстук-бабочка муж RAMSEY 100% полиэс', 30400, '2015-04-02 20:14:52', '2026-07-03 12:09:46', 30, '2026-06-19 13:18:20', '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0', 'Интернет-Магазин', '', '', '370', 0, '', '', '8', '', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,19,20', '2019-06-05 23:57:38', 0, 0, 0, 0, 0, 'ru', 'U0GFVHFFH', 0, 0, 'rub', 0, 0, '', 1, '1,9,11,12,13,15,17,18,14,10,16');
+
+TRUNCATE TABLE `users_actions`;
+
+TRUNCATE TABLE `users_backup_3003`;
+
+TRUNCATE TABLE `users_calls`;
+
+TRUNCATE TABLE `users_calls_incoming`;
+
+TRUNCATE TABLE `users_calls_log`;
+
+TRUNCATE TABLE `users_client_managers`;
+
+TRUNCATE TABLE `user_comments`;
+
