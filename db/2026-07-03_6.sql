53df3f0a8ce9bcf1abc3de98ea315855ce65e5ec
Nikolay <info@web-nick.com>
2026-07-03 15:45:27 +0300

    Поправить на снипет SVETLOV: https://github.com/kosatka-org/lsb/issues/50

---
+++ db/2026-07-03_6.sql
@@ -0,0 +1,5 @@
+UPDATE `cities` SET `meta_title` = 'Представительство Svetlov Jewelry в Москве' WHERE `id` = 1;
+
+UPDATE `cities` SET `meta_keywords` = 'Доставка в Москву' WHERE `id` = 1;
+
+UPDATE `cities` SET `meta_description` = 'Наш магазин осуществляет доставку в Москву и ближайшее Подмосковье' WHERE `id` = 1;
